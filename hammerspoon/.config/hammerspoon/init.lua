71c4867f27c5e35ccf53a4a9affab85230ce8300
Branden Garrett <branden.garrett@gmail.com>
2020-10-10 16:45:46 -0400

    Add hammerspoon window mananagement hotkeys

--- hammerspoon/.config/hammerspoon/init.lua
+++ hammerspoon/.config/hammerspoon/init.lua
@@ -24,6 +24,60 @@ longQuit = require "longQuit"
 
 hs.window.animationDuration = 0
 
+function moveLeftHalf()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end
+
+function moveRightHalf()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end
+
+function maximize()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f)
+end
+
+function moveToScreen(screenPos)
+  window = hs.window.focusedWindow()
+  screen = hs.screen.find({x=screenPos, y=0})
+  window:moveToScreen(screen)
+end
+
+hs.hotkey.bind(hyper, "1", function()
+  moveToScreen(0)
+  moveLeftHalf()
+end)
+
+hs.hotkey.bind(hyper, "2", function()
+  moveToScreen(0)
+  moveRightHalf()
+end)
+
+hs.hotkey.bind(hyper, "M", maximize)
+hs.hotkey.bind(hyper, "Return", maximize)
 -----------------------------------------------
 -- Hotkeys
 -----------------------------------------------
